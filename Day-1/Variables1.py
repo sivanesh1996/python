4b30cac677997c05c62342049ed3ca1c50038a30
sivanesh <vssivanesh@gmail.com>
2023-07-22 22:19:03 +0530

    global variable

---
+++ Day-1/Variables1.py
@@ -0,0 +1,23 @@
+x="siva"
+y=23
+z="male"
+print(x,y,z)
+
+a="python"
+b="is"
+c="easy"
+print(a+b+c)
+
+def myFunc():
+    x="Selva Kumar"
+    print("My name is: "+x)
+
+myFunc()
+
+def myFunc1():
+    global z
+    z="female"
+
+myFunc1()
+print("entered gender: "+z)
+
