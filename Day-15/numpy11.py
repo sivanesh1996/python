1335ad7dea223d799e9890e0c86d6c4155fe2cf3
sivanesh <vssivanesh@gmail.com>
2023-08-14 22:19:07 +0530

    permutation

---
+++ Day-15/numpy11.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy import random
+
+#random is an external module in numpy, so we must import
+
+#shuffle()
+
+a=np.array([1,2,3,4,5])
+random.shuffle(a)
+print(a)
+
+#given list is shuffled itself
+a1=np.array([11,22,33,44,55])
+random.permutation(a1) #given values not changed
+print(a1)
+
+b=random.permutation(a1)
+print(b)
