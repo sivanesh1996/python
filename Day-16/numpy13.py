45efacd5c8f8ced59c98d4ca6e0b26491fc7394b
sivanesh <vssivanesh@gmail.com>
2023-09-01 11:53:43 +0530

    distribution

--- Day-16/numpy13.py
+++ Day-16/numpy13.py
@@ -1,21 +1,21 @@
 from numpy import random
 
-x= random.normal(loc=2,scale=4,size=(2,3))
-print(x)
+# x= random.normal(loc=2,scale=4,size=(2,3))
+# print(x)
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 #using normal distribution in seaborn
-#sns.distplot(random.normal(loc=2,scale=4,size=(2,3)),hist=True)
-#plt.show()
+sns.distplot(random.normal(loc=2,scale=4,size=(2,3)),hist=True)
+plt.show()
 
 #binomial distribtion
 
-y=random.binomial(n=5,p=0.5,size=5)
-print('binomial distribution')
-print(y)
+# y=random.binomial(n=5,p=0.5,size=5)
+# print('binomial distribution')
+# print(y)
 
 #using binomial distribution in seaborn
-sns.distplot(random.binomial(n=5,p=0.2,size=5),hist=True)
-plt.show()
+# sns.distplot(random.binomial(n=5,p=0.2,size=5),hist=True)
+# plt.show()
